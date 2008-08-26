63a5824df8dfd0261f8906fed85f6e5c19220b03
collin <collintmiller@gmail.com>
2008-08-26 02:39:43 -0500

    more stuff

---
+++ public/bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:s=document.createElement('script');s.setAttribute('src','http://localhost:4567/embedded.js');document.getElementsByTagName('body')[0].appendChild(s);void(s);
---
+++ web.rb
@@ -0,0 +1,4 @@
+require 'gen'
+
+__DIR__ = Pathname.new(__FILE__).dirname.expand_path
+APP_ROOT = __DIR__
