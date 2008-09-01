26e4fc7c2c7cb79bf2e3a111997aa44d371ece30
collin <collintmiller@gmail.com>
2008-09-01 11:31:43 -0500

    revised x-domain strategy

--- gen.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'pathname'
-require 'sinatra/lib/sinatra'
-
-__DIR__ = Pathname.new(__FILE__).dirname.expand_path
-APP_ROOT = __DIR__
--- web.rb
+++ web.rb
@@ -1,4 +1,4 @@
-require 'gen'
+require 'lib/gen'
 
 __DIR__ = Pathname.new(__FILE__).dirname.expand_path
 APP_ROOT = __DIR__
