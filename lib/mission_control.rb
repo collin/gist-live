7bec14347d08de5d7d8c6709727b37399fe131a5
collin <collintmiller@gmail.com>
2008-09-01 11:32:38 -0500

    new x-domain strategy

---
+++ lib/mission_control.rb
@@ -0,0 +1,50 @@
+require 'gen'
+require 'legs'
+require 'uuid'
+require 'ostruct'
+
+Legs.start do
+  def initialize
+    @documents = {}
+    @editors = {}
+    @owners = {}
+  end
+
+  def begin_editing data, file_name
+    document = OpenStruct.new :name => file_name,
+      :id => UUID.new, :data => data
+    
+    @documents[document.id] = document
+    @editors[document.id] = [self]
+    @owners[document.id] = self
+    
+    document
+  end
+
+  def also_edit document_id
+    document = @documents[document_id]
+    @editors[document_id] << self
+    document
+  end
+  
+  def stop_editing document_id, reason="saved"
+    return false unless @owners[document_id] == self
+    for editor in @editors[document_id] - [self]
+      editor.notify!(:stopped, reason)
+    end
+    true
+  end
+  
+  
+  def diff document_id, patch
+    for editor in @editors[document_id] - [self]
+      editor.notify! :patch, patch
+    end
+    true
+  end
+  
+  private
+  def log?; true end
+end
+
+sleep
---
+++ src/embedded/gist_live.js
@@ -0,0 +1,52 @@
+jQuery.fn.gist_text = function() {
+  var $this = this;
+  var map = $this.parents('.file').find('.highlight .line').map(function(){return jQuery(this).text();
+  });
+  var text = ([]).join.call(map, "\n");
+  return text
+};
+function edit_link_events() {
+  var $this = edit_link
+  $this.bind("click", function(e) {
+    var $this = jQuery(this);
+    var gist_text = $this.gist_text();
+    (function($this) {
+      $this.prepend(live_edit_panel);
+      frames[0].name = gist_text
+      live_edit_panel.attr({src: 'http://localhost:8001//editor.html',id: 'live-edit-panel'
+      });
+})(jQuery("#gist_data"));
+});
+}
+edit_link_events();
+function append_live_edit_tools() {
+  var $this = $(this) 
+  $this.append(edit_link.clone(true));
+}
+(function($this) {
+  $this.append(welcome_dialog);
+  $this.prepend(styles);
+  (function($this) {
+    $this.each(append_live_edit_tools)  
+})($this.find("#files .actions"));
+})(jQuery("body"));
+(function($this) {
+  $this.hide();
+  $this.center();
+  $this.gradient({ from: '003366',to: '333333' ,position:'bottom'
+  });
+  (function($this) {
+    $this.css('opacity', 0.05);
+})($this.find(".gradient"));
+  $this.corner('wicked 20px');
+  function and_remove() {
+    $this.remove();
+}
+  function hide_welcome() {
+    $this.fadeOut(3000, and_remove);
+}
+  $this.fadeIn(1000, hide_welcome);
+  (function($this) {
+    $this.css('display', '');
+})(jQuery("#gist-live-welcome :hidden"));
+})(jQuery("#gist-live-welcome"));
